c10faa6de2377952d25be63b32326c987d30626b
isem0888 <semenov08808@gmail.com>
2026-06-10 21:31:00 +0300

    Update ConnectWallet.tsx

--- app/components/ConnectWallet.tsx
+++ app/components/ConnectWallet.tsx
@@ -4,11 +4,9 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function ConnectWallet() {
   return (
-    <div className="mt-8">
-      <ConnectButton
-        showBalance={false}
-        accountStatus="address"
-      />
-    </div>
+    <ConnectButton
+      showBalance={false}
+      accountStatus="address"
+    />
   );
 }
